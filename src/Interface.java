a66992d3373d785b995c1c1191bbba8add8f7d09
Texxi <gantenbein.michael@gmail.com>
2018-01-21 13:30:56 +0100

    V21

---
+++ src/Interface.java
@@ -0,0 +1,25 @@
+
+package src;
+
+import src.user.Admin;
+import src.user.Controller;
+import src.user.Kunde;
+
+public class Interface {
+	public static void main(String[] args) {
+
+		Admin ka = new Admin();
+		Controller kc = new Controller();
+		Kunde kk = new Kunde();
+
+		System.out.println(ka.check(2));
+		System.out.println(kc.analyze(1));
+		System.out.println(kk.place(123));
+		System.out.println(kk.read());
+
+
+		// HALLO UPDATE
+
+	}
+
+}
